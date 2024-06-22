6c7b85c5087b89fe68a85c77fd991178af4c19a7
Guido Molina <gmolina@clarika.com.ar>
2024-06-22 19:10:00 -0300

    add estructure page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./pages/header/header.component";
+import { BodyComponent } from "./pages/body/body.component";
+import { FooterComponent } from "./pages/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, BodyComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'wiki-anime';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,7 @@ export const routes: Routes = [
     {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
         {path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)},
         {path: 'header', loadChildren: () => import('./pages/header/header.module').then(m => m.HeaderModule)},
+        {path: 'body', loadChildren: () => import('./pages/body/body.module').then(m => m.BodyModule)},
+        {path: 'footer', loadChildren: () => import('./pages/footer/footer.module').then(m => m.FooterModule)},
         {path: '', redirectTo: '/home', pathMatch: 'full'}
     ];
