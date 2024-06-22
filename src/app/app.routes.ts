aa2d5362a0ea3ac24f58e463bbe980b9b8b73e38
Guido Molina <gmolina@clarika.com.ar>
2024-06-22 18:44:34 -0300

    add header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
         {path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule)},
+        {path: 'header', loadChildren: () => import('./pages/header/header.module').then(m => m.HeaderModule)},
         {path: '', redirectTo: '/home', pathMatch: 'full'}
     ];
